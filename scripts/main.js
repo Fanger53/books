5eb1d4e135b106db117cbd00d1594f95885d0163
Leonardo <fanger53@gmail.com>
2021-09-16 14:57:18 -0500

    add class storage with local storage

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,17 @@ class Book {
   }
 }
 
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
 
 
 
