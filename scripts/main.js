b567e1cbfc9f3e392820ffb423ec8a98ed7cc55e
Leonardo <fanger53@gmail.com>
2021-09-16 14:58:08 -0500

    add class storage with local storage

--- scripts/main.js
+++ scripts/main.js
@@ -19,5 +19,24 @@ class Store {
     return books;
   }
 
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 
 
