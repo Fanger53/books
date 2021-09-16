6c95bca5ec1066bade3ae291549484dc54aada8f
Leonardo <fanger53@gmail.com>
2021-09-16 15:02:18 -0500

    add dom listeners

--- scripts/main.js
+++ scripts/main.js
@@ -84,3 +84,30 @@ class UI {
   }
 }
 
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    const book = new Book(title, author, isbn);
+
+    UI.addBookToList(book);
+
+    Store.addBook(book);
+
+    UI.showAlert('Book Added', 'success');
+
+    UI.clearFields();
+  }
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  UI.showAlert('Book Removed', 'success');
+});
+
